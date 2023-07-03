41aa522b34be16413a1fef7625f85a256eea4df2
Denis Suprun <suprundenis@gmail.com>
2023-07-03 12:40:30 +0300

    + Minor fixes

--- src/pagepaths.h
+++ src/pagepaths.h
@@ -5,9 +5,7 @@
 
 namespace PagePaths {
     QString const aboutPage = "qml/pages/AboutPage.qml";
-    QString const aboutPage2 = "qml/pages/MainPage.qml";
     QString const mainPage = "qml/pages/MainPage.qml";
-    QString const mainPage2 = "qml/pages/MainPage.qml";
 }
 
 #endif // PAGEPATHS_H
