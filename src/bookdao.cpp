edb86fa0fa9dff538cbe536a50d833195c0aa218
Denis Suprun <suprundenis@gmail.com>
2023-07-21 18:43:43 +0300

    + Adding some DB logic to BookDao

--- src/bookdao.cpp
+++ src/bookdao.cpp
@@ -5,6 +5,185 @@ BookDao::BookDao()
 
 }
 
+void BookDao::openDb()
+{
+    auto db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("db.sqlite");
+
+    if (!db.open()) {
+       qDebug() << "Error: connection with database failed";
+    } else {
+       qDebug() << "Database: connection ok";
+    }
+
+    QSqlQuery query;
+    query.prepare("CREATE TABLE IF NOT EXISTS books ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "author TEXT NOT NULL,"
+        "title TEXT NOT NULL,"
+        "tp INTEGER NOT NULL,"
+        "position INTEGER NOT NULL);"
+    );
+    
+    if (!query.exec()) { qDebug() << "Create table error:" << query.lastError(); }
+}
+
+QList<BookModel> BookDao::getAllBooks()
+{
+    QSqlQuery query;
+    query.prepare("SELECT * FROM books ORDER BY position ASC;");
+
+    if (!query.exec()) { qDebug() << "Failed: " << query.lastError(); }
+
+    int idxId = query.record().indexOf("id");
+    int idxAuthor = query.record().indexOf("author");
+    int idxTitle = query.record().indexOf("title");
+    int idxTp = query.record().indexOf("tp");
+    int idxPosition = query.record().indexOf("position");
+
+    auto result = QList<BookModel>();
+
+    while (query.next()) 
+    {
+        auto id = query.value(idxId).toInt();
+        auto author = query.value(idxAuthor).toString();
+        auto title = query.value(idxTitle).toString();
+        auto tp = query.value(idxTp).toInt();
+        auto position = query.value(idxPosition).toInt();
+
+        result.append(BookModel(id, author, title, tp, position));
+    }
+    
+    return result;
+}
+
+void BookDao::insert(BookModel book)
+{
+    QSqlQuery query;
+
+    query.prepare(
+        "UPDATE books "
+        "SET position = position + 1 "
+        "WHERE position >= ?;"
+    );
+    query.addBindValue(book.position);
+    if (!query.exec()) { qDebug() << "Failed: " << query.lastError(); }
+
+    query.prepare(
+        "INSERT INTO books "
+        "(author, title, tp, position) "
+        "VALUES(?, ?, ?, ?);"
+    );
+    query.addBindValue(book.author);
+    query.addBindValue(book.title);
+    query.addBindValue(book.totalPages);
+    query.addBindValue(book.position);
+    if (!query.exec()) { qDebug() << "Failed: " << query.lastError(); }
+
+    auto insertId = query.lastInsertId().toInt();
+    book.id = insertId;
+
+    // beginInsertRows(QModelIndex(), book.position, book.position);
+    // books.insert(book.position, book);
+    // endInsertRows();
+
+    // int next = book.position + 1;
+    // int end = books.size();
+    // for (int i = next; i < end; i++) { books[i].position = i; }
+    // emit dataChanged(createIndex(next, 0), createIndex(end, 0));
+}
+
+void BookDao::remove(const int id, const int position)
+{
+    QSqlQuery query;
+    query.prepare(
+        "UPDATE books "
+        "SET position = position - 1 "
+        "WHERE position >= ?;"
+    );
+    query.addBindValue(position);
+    if (!query.exec()) { qDebug() << "Failed: " << query.lastError(); }
+
+    query.prepare("DELETE FROM books WHERE id = ?;");
+    query.addBindValue(id);
+    if (!query.exec()) { qDebug() << "Failed: " << query.lastError(); }
+
+    // beginRemoveRows(QModelIndex(), position, position);
+    // books.erase(books.begin() + position);
+    // endRemoveRows();
+
+    // int next = position;
+    // int end = books.size();
+    // for (int i = next; i < end; i++) { books[i].position = i; }
+    // emit dataChanged(createIndex(next, 0), createIndex(end, 0));
+}
+
+void BookDao::moveToTop(const int id, const int position)
+{
+    QSqlQuery query;
+    query.prepare(
+        "UPDATE books "
+        "SET position = position + 1 "
+        "WHERE position < ?;"
+    );
+    query.addBindValue(position);
+    if (!query.exec()) { qDebug() << "Failed: " << query.lastError(); }
+
+    query.prepare(
+        "UPDATE books "
+        "SET position = 0 "
+        "WHERE id = ?;"
+    );
+    query.addBindValue(id);
+    if (!query.exec()) { qDebug() << "Failed: " << query.lastError(); }
+
+    // beginMoveRows(QModelIndex(), position, position, QModelIndex(), 0);
+    // books.move(position, 0);
+    // endMoveRows();
+
+    // int next = 0;
+    // int end = position;
+    // for (int i = next; i <= end; i++) { books[i].position = i; }
+    // emit dataChanged(createIndex(next, 0), createIndex(end, 0));
+}
+
+void BookDao::update(const int id, const QString author, const QString title, const int totalPages, const int position)
+{
+    qDebug() << "Position: " << position;
+    QSqlQuery query;
+
+    query.prepare(
+        "UPDATE books "
+        "SET author = ?, title = ?, tp = ? "
+        "WHERE id = ?;"
+    );
+    query.addBindValue(author);
+    query.addBindValue(title);
+    query.addBindValue(totalPages);
+    query.addBindValue(id);
+    if (!query.exec()) { qDebug() << "Failed: " << query.lastError(); }
+
+    // books[position].author = author;
+    // books[position].title = title;
+    // books[position].totalPages = totalPages;
+
+    // int next = position;
+    // int end = position;
+    // emit dataChanged(createIndex(next, 0), createIndex(end, 0));
+}
+
+void BookDao::closeDb()
+{
+    {
+        // this additional scope is necessary, because allows
+        // to release db object after it was closed but before
+        // removeDatabase will be called
+        auto db = QSqlDatabase::database();
+        db.close();
+    }
+    QSqlDatabase::removeDatabase( QSqlDatabase::defaultConnection );
+}
+
 BookModel::BookModel(const int id, const QString author, const QString title, const int totalPages, const int position) {
     this->id = id;
     this->author = author;
--- src/bookdao.h
+++ src/bookdao.h
@@ -9,6 +9,15 @@ class BookDao
 {
 public:
     BookDao();
+
+    void openDb();
+    QList<BookModel> getAllBooks();
+    void insert(BookModel book);
+    void insert(const QString author, const QString title, const int totalPages, const int position);
+    void remove(const int id, const int position);
+    void moveToTop(const int id, const int position);
+    void update(const int id, const QString author, const QString title, const int totalPages, const int position);
+    void closeDb();
 };
 
 struct BookModel
